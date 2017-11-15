a2ac97b0ed31822c93408600ea1f8c2c4fdc6766
TopestKek <kondor1995@mail.ru>
2017-11-15 15:23:28 +0300

    Change default render class

--- chistomen/settings.py
+++ chistomen/settings.py
@@ -88,13 +88,10 @@ AUTHENTICATION_BACKENDS = (
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-'''
+
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-    ),
+    'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',)
 }
-'''
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
